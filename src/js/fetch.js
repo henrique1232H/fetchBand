fd1b01a60819b4b2553d8eec73a6c6d86177cb8b
henrique1232H <henriqueau111@gmail.com>
2024-01-22 00:41:31 -0300

    first commit

---
+++ src/js/fetch.js
@@ -0,0 +1,29 @@
+export default class Artist {
+   artistSelect = this.artistSelect;
+
+   artists = {
+        name: this.name,
+        count: this.count,
+        area: this.area,
+        lifeSpan: this.lifeSpan,
+        id: this.id
+   }
+   
+
+    
+    searchArtist() {
+        fetch(`https://musicbrainz.org/ws/2/artist?fmt=json&query=${this.artistSelect}`)
+            .then(data => data.json())
+            .then(data => data.artists)
+            .then(html => {
+                
+                this.artists.name = html[0].name;
+                this.artists.count = html[0].score;
+                this.artists.area = html[0].area.name;
+                this.artists.lifeSpan = html[0].area["life-span"].ended;
+                this.artists.id = html[0].id;
+            })
+
+    }
+
+}
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import Artist from "./fetch.js";
+
+const artist = new Artist();
+
+artist.searchArtist()
