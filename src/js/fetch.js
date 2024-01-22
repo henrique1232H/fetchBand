09d1865558b8534860fcaac2615ca9bbba7d8c34
henrique1232H <henriqueau111@gmail.com>
2024-01-22 13:55:22 -0300

    ajusts

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -7,24 +7,53 @@ export default class Artist {
         area: this.area,
         lifeSpan: this.lifeSpan,
         id: this.id
-   }
+   };
+
+   artistsWorks = []
    
 
     
     searchArtist() {
         fetch(`https://musicbrainz.org/ws/2/artist?fmt=json&query=${this.artistSelect}`)
             .then(data => data.json())
-            .then(data => data.artists)
-            .then(html => {
+            .then(data => data.artists[0])
+            .then(response => {
+                console.log(response)
                 
-                this.artists.name = html[0].name;
-                this.artists.count = html[0].score;
-                this.artists.area = html[0].area.name;
-                this.artists.lifeSpan = html[0].area["life-span"];
-                this.artists.id = html[0].id;
+                try {
+                    this.artists.name = response.name;
+                    this.artists.count = response.score;
+                    this.artists.area = response.area.name;
+                    this.artists.lifeSpan = response.area["life-span"];
+                    
+                } catch (error) {
+                    
+                    console.log(error)
+                }
+                this.artists.id = response.id;
+                
 
-                console.log(this.artists)
+                document.querySelectorAll("h2")[0].innerHTML = `O nome completo é ${this.artists.name}`;
+                document.querySelectorAll('h2')[1].innerHTML = `A contagem é ${this.artists.count}`;
+                document.querySelectorAll("h2")[2].innerHTML = `Ele veio de ${this.artists.area}`;
+        
+                this.searchArtistWork();
+                
+                console.log(this.artists);
             })
+    }
+
+    searchArtistWork() {
+        fetch(`https://musicbrainz.org/ws/2/release-group?fmt=json&artist=${this.artists.id}`)
+         .then(data => data.json())
+         .then(response => {
+            this.artistsWorks.push(response["release-groups"])
+
+            for(let i = 0; i < this.artistsWorks.length;i++) {
+                console.log(this.artistsWorks[i])
+            }
+         })
+
     }
 
 }
--- src/js/form.js
+++ src/js/form.js
@@ -3,22 +3,19 @@ import Artist from "./fetch.js";
 export const form = () => {
     const form = document.querySelector("form");
     const input = form.querySelector("input");
-    const artist = new Artist()
+    const artist = new Artist();
 
     form.addEventListener("submit", (e) => {
         e.preventDefault()
+
+        if(input.value === "") {
+            return
+        }
         
         artist.artistSelect = input.value;
 
-        artist.searchArtist();
+        artist.searchArtist()
 
-        console.log(artist.artists);
-
-        document.querySelectorAll("h1")[1].innerHTML = `O nome completo é ${artist.artists.name}`;
-        document.querySelector('h2').innerHTML = `A contagem é ${artist.artists.count}`;
-        document.querySelector("h3").innerHTML = `Ele veio de ${artist.artists.area}`;
-
-        
         
     })
 }
