a6226c7e7369b3b6229cc83d6115cb0aaf0b88e9
henrique1232H <henriqueau111@gmail.com>
2024-01-24 00:23:16 -0300

    add type

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,102 +1,111 @@
 export default class Artist {
-   artistSelect = this.artistSelect;
+  artistSelect = this.artistSelect;
 
-   artists = {
-        name: this.name,
-        count: this.count,
-        area: this.area,
-        lifeSpan: this.lifeSpan,
-        id: this.id
-   };
+  artists = {
+    name: this.name,
+    count: this.count,
+    area: this.area,
+    lifeSpan: this.lifeSpan,
+    id: this.id,
+  };
 
-   artistsWork = {
-        works: [],
-        launch: []
-   };
-       
-    searchArtist() {
-        document.querySelector(".loading").classList.remove("hidden");
-        document.querySelector(".response").classList.add("hidden");
+  artistsWork = {
+    works: [],
+    type: [],
+    launch: [],
+  };
 
-        fetch(`https://musicbrainz.org/ws/2/artist?fmt=json&query=${this.artistSelect}`)
-            .then(data => data.json())
-            .then(data => data.artists[0])
-            .then(response => {           
-                document.querySelector(".loading").classList.add("hidden");
-                document.querySelector(".response").classList.remove("hidden");
-                document.querySelector(".more").classList.remove("hidden");
-                     
-                try {
-                    this.artists.name = response.name;
-                    this.artists.count = response.score;
-                    this.artists.area = response.area.name;
-                    this.artists.lifeSpan = response.area["life-span"];
-                    
-                } catch (error) {
-                    console.log(error)
-                }
-                this.artists.id = response.id;
-                
-                this.showMessages()
-                this.searchArtistWork();
-                this.removeTable()
-                
-            })
-    }
+  searchArtist() {
+    document.querySelector(".loading").classList.remove("hidden");
+    document.querySelector(".response").classList.add("hidden");
 
-    searchArtistWork() {
-        fetch(`https://musicbrainz.org/ws/2/release-group?fmt=json&artist=${this.artists.id}`)
-         .then(data => data.json())
-         .then(response => {
-            const releaseGroups = response["release-groups"];
+    fetch(
+      `https://musicbrainz.org/ws/2/artist?fmt=json&query=${this.artistSelect}`
+    )
+      .then((data) => data.json())
+      .then((data) => data.artists[0])
+      .then((response) => {
+        document.querySelector(".loading").classList.add("hidden");
+        document.querySelector(".response").classList.remove("hidden");
+        document.querySelector(".more").classList.remove("hidden");
 
-            for(let i = 0; i < releaseGroups.length;i++) {
-                this.artistsWork.works.push(releaseGroups[i].title);
-                this.artistsWork.launch.push(releaseGroups[i]["first-release-date"])
-            }
-            this.createTable();
-         })
+        try {
+          this.artists.name = response.name;
+          this.artists.count = response.score;
+          this.artists.area = response.area.name;
+          this.artists.lifeSpan = response.area["life-span"];
+        } catch (error) {
+          console.log(error);
+        }
+        this.artists.id = response.id;
 
-    }
+        this.showMessages();
+        this.searchArtistWork();
+        this.removeTable();
+      });
+  }
 
-    showMessages() {
-        document.querySelectorAll("h2")[0].innerHTML = `O nome completo é ${this.artists.name}`;
-        document.querySelectorAll('h2')[1].innerHTML = `A contagem é ${this.artists.count}`;
-        document.querySelectorAll("h2")[2].innerHTML = `Ele veio de ${this.artists.area}`;
-    }
+  searchArtistWork() {
+    fetch(
+      `https://musicbrainz.org/ws/2/release-group?fmt=json&artist=${this.artists.id}`
+    )
+      .then((data) => data.json())
+      .then((response) => {
+        const releaseGroups = response["release-groups"];
 
-    removeResponseAndMore() {
-        document.querySelector(".response").classList.add("hidden");
-        document.querySelector(".more").classList.add("hidden");
-    }
-
-    createTable() {
-        const tbody = document.querySelector("tbody");
-
-       
-        for(let i = 0; i < this.artistsWork.works.length;i++) {
-            const tr = document.createElement("tr");
-            const name = document.createElement("td");
-            const release = document.createElement("td")
-
-            tr.appendChild(name)
-            tr.appendChild(release)
-
-            name.innerHTML = this.artistsWork.works[i];
-            release.innerHTML = this.artistsWork.launch[i]
-
-            tbody.appendChild(tr)
+        for (let i = 0; i < releaseGroups.length; i++) {
+          this.artistsWork.works.push(releaseGroups[i].title);
+          this.artistsWork.launch.push(releaseGroups[i]["first-release-date"]);
+          this.artistsWork.type.push(releaseGroups[i]["primary-type"])
         }
 
+        this.createTable();
+      });
+  }
 
+  showMessages() {
+    document.querySelectorAll(
+      "h2"
+    )[0].innerHTML = `O nome completo é ${this.artists.name}`;
+    document.querySelectorAll(
+      "h2"
+    )[1].innerHTML = `A contagem é ${this.artists.count}`;
+    document.querySelectorAll(
+      "h2"
+    )[2].innerHTML = `Ele veio de ${this.artists.area}`;
+  }
+
+  removeResponseAndMore() {
+    document.querySelector(".response").classList.add("hidden");
+    document.querySelector(".more").classList.add("hidden");
+  }
+
+  createTable() {
+    const tbody = document.querySelector("tbody");
+    
+    for (let i = 0; i < this.artistsWork.works.length; i++) {
+      const tr = document.createElement("tr");
+      const name = document.createElement("td");
+      const type = document.createElement("td");
+      const release = document.createElement("td");
+
+      tr.appendChild(name);
+      tr.appendChild(type)
+      tr.appendChild(release);
+
+      name.innerHTML = this.artistsWork.works[i];
+      type.innerHTML = this.artistsWork.type[i];
+      release.innerHTML = this.artistsWork.launch[i];
+
+      tbody.appendChild(tr);
     }
+  }
 
+  removeTable() {
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
 
-    removeTable() {
-        const tbody = document.querySelector("tbody");
-        tbody.innerHTML = ""
-
-        this.artistsWork.works = [];
-        this.artistsWork.launch = [];
-    }
+    this.artistsWork.works = [];
+    this.artistsWork.launch = [];
+  }
 }
